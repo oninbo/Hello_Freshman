16df6c347f770c69cdf734cbcaa96752784dea23
Anton Skudarnov <Anton Skudarnov>
2017-04-28 18:14:56 +0300

    Working version with fucntions

--- bot.py
+++ bot.py
@@ -1,13 +1,9 @@
 import telebot
 from player import Player
-import states
+from states import states
 import time
 import config
-from states import states
 from telebot import types
-from typing import *
-from state import State
-import states
 from contentUnit import ContentUnit
 
 bot = telebot.TeleBot(config.token)
@@ -15,8 +11,6 @@ bot = telebot.TeleBot(config.token)
 players = {}
 
 delay = 0.5
-
-
 
 
 @bot.message_handler(commands=['start'])
@@ -28,15 +22,17 @@ def start_game(message):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def switch_state(message):
-    player:Player = players[message.chat.id]
+
+    player: Player = players[message.chat.id]
     current_state = player.current_state
     if current_state.callback:
         current_state.callback()
-    next_state = current_state.get_next_state(message, player)
-    player.current_state = states[next_state]
+    change_state(message)
     show_content(player)
 
-contentFunctions = {"text":bot.send_message,"photo":bot.send_photo}
+
+contentFunctions = {"text": bot.send_message, "photo": bot.send_photo}
+
 
 def show_content(player):
     content: ContentUnit = player.current_state.content
@@ -45,12 +41,19 @@ def show_content(player):
     for key in buttons.keys():
         markup.add(key)
     for contentUnit in content:
-        contentFunctions[contentUnit.type](player.id,contentUnit.value,reply_markup = markup)
+        contentFunctions[contentUnit.type](player.id, contentUnit.value, reply_markup=markup)
         time.sleep(delay)
 
 
+def change_state(message):
+    player = players[message.chat.id]
+    current_state = player.current_state
+    buttons = current_state.buttons
 
-
+    if (message.text in buttons):
+        player.current_state = states[buttons[message.text]]
+    else:
+        player.current_state = current_state.default_children
 
 
 
--- state.py
+++ state.py
@@ -12,13 +12,3 @@ class State:
         self.buttons = buttons
         self.default_children = default_children
 
-    def get_next_state(self,message, player):
-
-        # for foo in current_state.content.callback:
-        #   foo(bot, player)
-
-        if (message.text in self.buttons.keys()):
-            next_state = self.buttons[message.text]
-        else:
-            next_state = self.default_children
-        return next_state
