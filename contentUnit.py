78af16437a92676013b841ecc8f596bc410f0d0c
Elena Lebedeva <oninbo@gmail.com>
2017-05-19 19:38:03 +0300

    changed default delay

--- contentUnit.py
+++ contentUnit.py
@@ -1,5 +1,5 @@
 class ContentUnit:
-    def __init__(self, type, value, delay=1):
+    def __init__(self, type, value, delay=2):
         self.type = type
         self.value = value
         self.delay = delay
