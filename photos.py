9cd0160fb5896f60fbbb8cda47ce2800fe8479c2
Anton Skudarnov <Anton Skudarnov>
2017-05-30 18:02:26 +0600

    7 day completed, photo added, remarks done

--- photos.py
+++ photos.py
@@ -5,7 +5,29 @@ urls = {
     "eating7": "http://savepic.ru/14089710.jpg",
     "eating5": "http://savepic.ru/14029295.jpg",
     "kombinat": "http://sportgyms.ru/uploads/posts/2014-10/1413369414_01.jpg",
+    "shuttle":"http://savepic.ru/14189662.jpg",
     "korpus": "http://savepic.ru/14038287.jpg",
+    "koridor":"http://savepic.ru/14056667.jpg",
+    "kitchen":"http://savepic.ru/14216275.jpg",
+    "bedroom":"http://savepic.ru/14211155.jpg",
+    # ISA:
+    "kamill":"http://savepic.ru/14168147.jpg",
+    "nikita":"http://savepic.ru/14188626.jpg",
+    "anton":"http://savepic.ru/14193746.jpg",
+    "evgeniy":"http://savepic.ru/14181458.jpg",
+    "andrey":"http://savepic.ru/14174290.jpg",
+    # -----------
+
+    # III day:
+    "lecture":"http://savepic.ru/14208085.png",
+    "sport_complex":"http://savepic.ru/14178389.jpg",
+    "techno_park":"http://savepic.ru/14163029.png",
+    "med_center":"http://savepic.ru/14219348.jpg",
+    # --------
+
+    #IV day:
+    "hoody":"http://savepic.ru/14182484.jpg",
+    #-------------
     "stairs": "http://savepic.ru/14082305.jpg",
     "cafeteria": "http://savepic.ru/14056667.jpg",
     "halloween":"http://savepic.ru/14116437.jpg",
@@ -13,7 +35,12 @@ urls = {
     "student_day":"http://savepic.ru/14130772.jpg",
     "valentine_day":"http://savepic.ru/14102100.jpg",
     "23/8":"http://savepic.ru/14104148.jpg",
-    "IAW":"http://savepic.ru/14104148.jpg",
+    "IAW":"http://savepic.ru/14167110.jpg",
     "slippers":"http://savepic.ru/14149207.jpg",
+
+    #VII day:
+    "mangal":"http://savepic.ru/14211159.jpg",
+    "sunset":"http://savepic.ru/14173271.jpg",
+    #-----------
 
 }
